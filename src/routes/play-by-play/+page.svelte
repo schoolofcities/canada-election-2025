4dbe448be10ecea1abe1f11382e8a190403a920c
an-k45 <an_k27@protonmail.com>
2025-04-23 10:41:18 -0400

    Structure website

---
+++ src/routes/play-by-play/+page.svelte
@@ -0,0 +1,35 @@
+<script>
+    import '../../assets/global-styles.css';
+
+    import TopSofC from '$lib/TopSofC.svelte';
+</script>
+
+<main>
+    <TopSofC/>
+
+    <div class="title">
+        <h1>Canada Votes 2025: A Play-by-Play in the Cities</h1>
+        <p>
+            <a href='https://www.linkedin.com/in/aniket-k-8a8b9921b/' target='_blank'>Aniket Kali</a>,
+            <a href='https://jamaps.github.io/' target='_blank'>Jeff Allen</a>
+            |
+            April 2025
+        </p>
+    </div>
+
+    <div class="text">
+        <p>
+            hello world
+        </p>
+    </div>
+    
+    <div class="container">
+        <p>This is where a component will go if we want it to be contained in width.</p>
+    </div>
+    
+    <p>This is where a component will go if we wanted to span the full page.</p>
+</main>
+
+<style>
+
+</style>
